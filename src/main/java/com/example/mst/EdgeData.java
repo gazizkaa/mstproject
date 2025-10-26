ba60686a7946c7391f316e5e813dc36f305ffe23
Gaziza Bakir <gazizabakir@Gazizas-MacBook-Pro.local>
2025-10-26 18:47:13 +0500

    Updated EdgeData.java with toString() for readable output

--- src/main/java/com/example/mst/EdgeData.java
+++ src/main/java/com/example/mst/EdgeData.java
@@ -17,4 +17,9 @@ public class EdgeData {
     public int getWeight() {
         return weight;
     }
+
+    @Override
+    public String toString() {
+        return from + " - " + to + " : " + weight;
+    }
 }
